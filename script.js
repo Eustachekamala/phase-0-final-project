b6fcd3ac5614d1740b34122ff72d6a2ccefb6911
eustachekamala <eustachejared25@gmail.com>
2024-06-21 19:14:25 +0300

    I did the profil of the portfolio and I did some update in our style of css

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sign_in = document.getElementById("container-sign_In");
-const btn_connect = document.getElementById("link");
 const dashboard_portfolio = document.getElementById("container-info-portfolio");
-btn_connect.addEventListener("click", () => {
-    sign_in.setAttribute("hidden", "");
-    dashboard_portfolio.removeAttribute("hidden");
-})
+
+function sign_In(){
+        sign_in.setAttribute("hidden", "");
+        dashboard_portfolio.removeAttribute("hidden");
+}
