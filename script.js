11f3f93f02ec0c1166ed16c9ac10123357cee4cd
eustachekamala <eustachejared25@gmail.com>
2024-06-22 09:45:38 +0300

    responsive for my portfolio

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const sign_in = document.getElementById("container-sign_In");
 const dashboard_portfolio = document.getElementById("container-info-portfolio");
+const menu = document.getElementById("menu");
+const contact = document.getElementById("contact-section");
 
 function sign_In(){
         sign_in.setAttribute("hidden", "");
         dashboard_portfolio.removeAttribute("hidden");
 }
+
+menu.addEventListener("click", () =>{
+        contact.style.visibility = "visible";
+});
