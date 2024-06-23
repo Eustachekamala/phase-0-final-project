3e3ae8935c68518a8a8e9484ed4df11a757b5084
eustachekamala <eustachejared25@gmail.com>
2024-06-23 18:28:48 +0300

    reposnive for the mobile phone, I did the reponsive of the nav bar

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const sign_in = document.getElementById("container-sign_In");
 const dashboard_portfolio = document.getElementById("container-info-portfolio");
 const menu = document.getElementById("menu");
 const contact = document.getElementById("contact-section");
+const links = document.getElementsByClassName("Icon-Dev");
 
 function sign_In(){
         sign_in.setAttribute("hidden", "");
@@ -10,4 +11,7 @@ function sign_In(){
 
 menu.addEventListener("click", () =>{
         contact.style.visibility = "visible";
+        if(contact.click(links)){
+                contact.style.visibility = "none"; 
+        }
 });
